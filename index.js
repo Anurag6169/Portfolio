515c366ee35e251320aec026d4e2e993ed5062eb
Anurag <anuragsharma6169@gmail.com>
2023-09-03 20:32:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+require('dotenv').config();
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const PORT = 5000;
+const app = express();
+
+// set for public access
+app.use(express.static(__dirname + "/public"));
+
+// set for ejs views for response
+app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+app.get("/portfolio-details1",(req,res)=>{
+    res.render("portfolio-details1");
+})
+
+app.get("/portfolio-details2",(req,res)=>{
+    res.render("portfolio-details2");
+})
+
+app.get("/portfolio-details3",(req,res)=>{
+    res.render("portfolio-details3");
+})
+
+app.post('/contact',(req,res)=>{
+    
+    console.log(req.body);  
+
+    const receiverClient = "anuragsharma6100@gmail.com";
+    const subjectClient = "New Contact from portfolio site";
+    const htmlClient = `<p>
+    Contact's details: <br/>
+        name: ${req.body.name}<br/>
+        email: ${req.body.email}<br/>
+        subject: ${req.body.subject}<br/>
+        message: ${req.body.message}<br/>
+    </p>`;
+    const transporter = nodemailer.createTransport({
+        service: "hotmail",
+        auth: {
+            user: process.env.OUTLOOK_EMAIL,
+            pass: process.env.OUTLOOK_PASSWORD,
+        },
+    });
+    const mailOptions = {
+        from: process.env.OUTLOOK_EMAIL, // sender address
+        to: receiverClient, // list of receivers
+        subject: subjectClient,
+        html:htmlClient,
+    };
+
+    transporter.sendMail(mailOptions,async (err,info)=>{
+        if(err) throw Error(err);
+        console.log(info);
+        res.redirect("/");
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}...`);
+});
